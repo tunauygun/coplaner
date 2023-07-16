8917bf730a7d1f9fbec3656bab91ea8ddb4a9bcc
Tuna Uygun <uyguntuna2018@gmail.com>
2023-07-16 17:01:19 -0400

    Update port config

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const {TimetableCreator} = require("./TimetableCreator");
 const courseData = require('./courses.json');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
