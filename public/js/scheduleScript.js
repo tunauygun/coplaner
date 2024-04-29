a284f6a7df4fec15555d3cedc184c6418a56fa3c
Tuna Uygun <uyguntuna2018@gmail.com>
2024-04-29 16:12:41 -0400

    Updated to vertical timetable view

--- public/js/scheduleScript.js
+++ public/js/scheduleScript.js
@@ -2,64 +2,48 @@ let schedule = {
   initialize: function(){
     schedule.activities.set();
     
-  }, 
+  },
   options: {
-    schedule: '#schedule', 
-    breaks: [0,0,0,0,0,0,0,0,0], // breaks duration
-    s_breaks: [475,525, 575, 630, 695, 750, 815, 870, 920], // the time after which the break begins
-    lesson_time: 60, // lesson duration (minutes)
-    lessons: 15, // number of lessons per week
-    start: function(){ // start at 7.10 
+    schedule: '#schedule',
+    start: function(){ // start at 7.10
       return schedule.general.toMin(8,0)
-    }, 
-    end: function(){ // start at 16.10 
-      return schedule.general.toMin(23,0)
     },
-    h_width: $('.s-hour-row').width(), // get a width of hour div
+    end: function(){ // start at 16.10
+      return schedule.general.toMin(22,0)
+    },
+    h_height: $('.s-hour-row').height(), // get a width of hour div
     minToPx: function(){ // divide the box width by the duration of one lesson
-      return schedule.options.h_width / schedule.options.lesson_time;
+      return schedule.options.h_height / 60;
     },
   },
   general: {
     hoursRegEx: function(hours){
-      var regex = /([0-9]{1,2}):([0-9]{1,2})-([0-9]{1,2}):([0-9]{1,2})/;
+      var regex = /([0-9]{1,2}).([0-9]{1,2})-([0-9]{1,2}).([0-9]{1,2})/;
       if(regex.test(hours)){
         return true;
       }else{
         return false;
       }
-      
+
     },
-    toMin: function(hours, minutes, string){ 
+    toMin: function(hours, minutes, string){
       // change time format (10,45) to minutes (645)
       if(!string){
         return (hours * 60) + minutes;
       }
-      
+
       if(string.length>0){
-        // "7.10"
+        // "7:10"
         var h = parseInt(string.split(':')[0]),
             m = parseInt(string.split(':')[1]);
-        
+
         return schedule.general.toMin(h, m);
       }
     },
     getPosition: function(start, duration, end){
-      var translateX = (start - schedule.options.start()) * schedule.options.minToPx(),
-          width = duration * schedule.options.minToPx(),
-          breaks = schedule.options.breaks,
-          s_breaks = schedule.options.s_breaks;
-      
-      $.each(breaks, function(index, item) { 
-        if( start < s_breaks[index] && duration > item && end > (s_breaks[index]+item) ){
-          width -= item * schedule.options.minToPx();
-        }
-        if( start > s_breaks[index] && duration > item && end > (s_breaks[index]+item) ){
-          translateX -= item * schedule.options.minToPx();
-        }
-      }); 
-      
-      return [translateX, width];
+      var translateY = (start - schedule.options.start()) * schedule.options.minToPx(),
+          height = duration * schedule.options.minToPx()
+      return [translateY-4, height];
     }
   },
   activities: {
@@ -72,7 +56,7 @@ let schedule = {
     add: function(week, lesson, hours, classroom, group, teacher, color){
       /* EXAMPLES --> week: 0-4, lesson: "Math", hours: "9.45-12.50", 
       classroom: 145, group: "A", teacher: "A. Badurski", color: "orange" */
-      let tab = "<div class='s-act-tab "+ color +"' data-hours='"+ hours +"'>\
+      var tab = "<div class='s-act-tab "+ color +"' data-hours='"+ hours +"'>\
             <div class='s-act-name'>"+ lesson +"</div>\
             <div class='s-wrapper'>\
               <div class='s-act-teacher'>"+ teacher +"</div>\
@@ -86,33 +70,51 @@ let schedule = {
     set: function(){
       $(schedule.options.schedule + ' .s-act-tab').each(function(i){
         var hours = $(this).attr('data-hours').split("-"),
-            start = /* HOURS */ parseInt(hours[0].split(":")[0]*60) 
-            + /* MINUTES */ parseInt(hours[0].split(":")[1]),
-            end = /* HOURS */ parseInt(hours[1].split(":")[0]*60) 
-            + /* MINUTES */ parseInt(hours[1].split(":")[1]),
+            start = /* HOURS */ parseInt(hours[0].split(":")[0]*60)
+                + /* MINUTES */ parseInt(hours[0].split(":")[1]),
+            end = /* HOURS */ parseInt(hours[1].split(":")[0]*60)
+                + /* MINUTES */ parseInt(hours[1].split(":")[1]),
             duration = end - start,
-            translateX = schedule.general.getPosition(start,duration,end)[0],
-            width = schedule.general.getPosition(start,duration,end)[1];
+            translateY = schedule.general.getPosition(start,duration,end)[0],
+            height = schedule.general.getPosition(start,duration,end)[1];
 
         $(this)
-          .attr({"data-start": start, "data-end": end})
-          .css({"transform": "translateX("+translateX+"px)", "width": width+"px"});
+            .attr({"data-start": start, "data-end": end})
+            .css({"transform": "translateY("+translateY+"px)", "height": height+"px"});
       });
     },
     update: function() {
       schedule.activities.clear();
       $("#title").text(`Schedule ${schedule.index+1}/${schedule.len}`);
-      for(const c of schedule.timetables[schedule.index].courses){
-        for(const ctl of c.ctls){
-          for(const day of ctl.dayInts){
-            if(ctl.room === "LINE") {
-              ctl.room = "Online"
-            }
-            schedule.activities.add(day, c.subject + " " + c.courseCode , ctl.startTime + "-" + ctl.endTime, ctl.room, c.section, c.instructor, "red");
-          }
-        }
+
+      const convertDaysOfWeekToNumbers = function (dayString) {
+        const dayMap = {
+          "mon": 0,
+          "tue": 1,
+          "wed": 2,
+          "thu": 3,
+          "fri": 4
+        };
+        const lowercaseDayString = dayString.toLowerCase().trim();
+        return dayMap[lowercaseDayString];
       }
-      updateUnscheduledCourseList()
+
+      for(const c of schedule.timetables[schedule.index]) {
+        schedule.activities.add(convertDaysOfWeekToNumbers(c.day), c.subject + " " + c.number , c.start_time + "-" + c.end_time, "", c.section, c.instructor, "red");
+      }
+
+      // for(const c of schedule.timetables[schedule.index].courses){
+      //   for(const ctl of c.ctls){
+      //     for(const day of ctl.dayInts){
+      //       if(ctl.room === "LINE") {
+      //         ctl.room = "Online"
+      //       }
+      //       schedule.activities.add(day, c.subject + " " + c.courseCode , ctl.startTime + "-" + ctl.endTime, ctl.room, c.section, c.instructor, "red");
+      //     }
+      //   }
+      // }
+
+      // updateUnscheduledCourseList()
     }
   }
   
